89e24e848761b13277661bff6430cbda0cf9acbc
Hashmmath <106550130+Hashmmath@users.noreply.github.com>
2023-03-24 23:43:25 +0530

    Delete Gmail-Slicer.py

--- Gmail-Slicer.py
+++
@@ -1,8 +0,0 @@
-email = input("Enter your Gmail address: ")
-if email.endswith("@gmail.com"):
-    username = email[:email.index("@")]
-    domain = "gmail.com"
-    print("Your username is:", username)
-    print("Your domain is:", domain)
-else:
-    print("Invalid Gmail address.")
